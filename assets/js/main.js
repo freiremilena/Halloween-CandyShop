68e8f4fa1bb60ca1a38e927ac0f508d88f00295d
freiremilena <fcamilenafr@gmail.com>
2021-11-01 15:26:56 -0300

    add category

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,11 @@ let homeSwiper = new Swiper(".home-swiper", {
     clickable: true,
   },
 });
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
